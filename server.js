c77450be25fac259eadf6e7762461d32d4bdd95b
Steve Womack <Steve.Womack@wwt.com>
2015-09-17 10:26:57 -0500

    Init

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 // server.js
 
-// set up ========================
-var express  = require('express');
-var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+// Load the http module to create an http server.
+var http = require('http');
+//
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "application/json"});
+  if (process.env.VCAP_SERVICES) {
+    response.write(process.env.VCAP_SERVICES);
+  }
+  else {
+    response.write('{}');
+  }
+  response.end();
+});
 
-// configuration =================
+var port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
-
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-
-// listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+server.listen(port);
+console.log('App listening on port: ', port);
+console.log('Services: ', process.env.VCAP_SERVICES);
